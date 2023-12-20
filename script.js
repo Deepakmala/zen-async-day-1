806a45aa9ec0a85e7f072be60568fd08fe36df3c
Deepakmala <123050483+Deepakmala@users.noreply.github.com>
2023-12-20 17:39:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+// Function to display countdown and message
+const displayCountdown = () => {
+  let count = 10;
+
+  // Countdown function
+  const countdown = (callback) => {
+    setTimeout(() => {
+      if (count >= 1) {
+        document.body.innerHTML = `<p>${count}</p>`;
+        count--;
+        countdown(callback);
+      } else {
+        document.body.innerHTML = ""; // Clear the display
+        setTimeout(callback, 1000);
+      }
+    }, 1000);
+  };
+
+  // Final message function
+  const displayMessage = () => {
+    document.body.innerHTML = "<p>Happy Independence Day!</p>";
+  };
+
+  // Start the countdown
+  countdown(displayMessage);
+};
+
+// Call the main function
+displayCountdown();
